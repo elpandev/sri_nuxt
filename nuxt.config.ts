88d7f7f710a343dcb3f732c808adea56c2c0891a
elpandev <javoavmystery@gmail.com>
2024-09-21 18:07:06 -0500

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,9 +4,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   runtimeConfig: {
-    // The private keys which are only available server-side
-    apiSecret: '123',
-    // Keys within public are also exposed client-side
     public: {
       apiBase: 'http://127.0.0.1:8787'
     }
