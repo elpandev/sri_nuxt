0543ce7540495f064dae194e116bfa3b9cc50561
elpandev <javoavmystery@gmail.com>
2024-10-02 02:04:48 -0500

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,11 +12,9 @@ export default defineNuxtConfig({
     }
   },
 
-  runtimeConfig: {
-    public: {
-      apiBase: 'http://127.0.0.1:8787'
-    }
-  },
+  css: [
+    '~/assets/scss/variables.scss'
+  ],
 
   compatibilityDate: '2024-09-08',
 })
