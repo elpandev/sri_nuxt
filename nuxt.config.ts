3021753fd5de5b495291c85bc300db046a2f68be
elpandev <javoavmystery@gmail.com>
2024-10-02 01:59:41 -0500

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
+
   runtimeConfig: {
     public: {
       apiBase: 'http://127.0.0.1:8787'
     }
-  }
+  },
+
+  compatibilityDate: '2024-09-08',
 })
